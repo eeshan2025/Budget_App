bb27c2e3ba5264628c36e9018406a0093f88394d
Eeshan khan <khaneeshan04@gmail.com>
2025-09-26 18:16:40 +0530

    added user

---
+++ User.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    
+    @Column(unique = true)
+    private String email;
+    
+    private String password;
+    private String role = "USER";
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+    // Constructors, getters, setters...
+}
